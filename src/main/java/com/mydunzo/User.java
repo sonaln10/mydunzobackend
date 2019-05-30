a234f840e9e990340026a41903dc800d93e610e2
Gupta <Aditya.Gupta@int.asurion.com>
2019-05-30 21:50:03 +0530

    Adding code

---
+++ src/main/java/com/mydunzo/User.java
@@ -0,0 +1,65 @@
+package com.mydunzo;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="appusers")
+public class User implements Serializable{
+	
+	
+	private static final long serialVersionUID = -1629956870851946039L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String userId;
+	private String password;
+	private String username;
+	private String role;
+	
+	public User(String userId, String password, String username, String role) {
+		super();
+		this.userId = userId;
+		this.password = password;
+		this.username = username;
+		this.role = role;
+	}
+	
+	
+	
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+}
