550c436ae4c9fa949720aa1735798a4a7766663e
sonaln10.SonalNikhade <sonaln10.sonal.nikhade10@gmail.com>
2019-06-10 15:54:37 +0530

    made changes for swagger

--- src/main/java/com/mydunzo/LoginController.java
+++ src/main/java/com/mydunzo/LoginController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class LoginController {
 	
 	@Autowired
@@ -16,6 +18,7 @@ public class LoginController {
 	@GetMapping("/findall")
 	public List<User> findAll(){
 	List<User> users = repository.findAll();
+	System.out.println("userList: "+users);
 	return users;
 	}
 }
